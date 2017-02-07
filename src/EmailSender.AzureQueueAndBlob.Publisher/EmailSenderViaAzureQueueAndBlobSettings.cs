b8bce81ec79717a3b2204df3ae602af9c98932b0
amigin <amigin@gmail.com>
2017-02-07 21:30:29 +0200

    Add project files.

---
+++ src/EmailSender.AzureQueueAndBlob.Publisher/EmailSenderViaAzureQueueAndBlobSettings.cs
@@ -0,0 +1,38 @@
+﻿using Common;
+using Common.Log;
+using Lykke.EmailSender;
+using Lykke.Integration.AzureQueueAndBlobs;
+using Lykke.Integration.AzureQueueAndBlobs.Publisher;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmailSender.AzureQueueAndBlob.Publisher
+{
+    public static class EmailSenderViaAzureQueueAndBlobBinding
+    {
+        public static EmailSenderViaQueue UseEmailSenderViaAzureQueueAndBlobPublisher(this IServiceCollection serviceCollection, 
+            AzureQueueAndBlobIntegrationSettings settings, ILog log)
+        {
+            var appName = "";
+
+            var publisher = new AzureQueuePublisher<EmailModel>(appName, settings)
+                .SetLogger(log)
+                .SetSerializer(new EmailSenderViaAzureQueueAndBlobSerializer())
+                .Start();
+
+            var emailSenderInstance = new EmailSenderViaQueue(publisher);
+
+            serviceCollection.AddSingleton<IEmailSender>(emailSenderInstance);
+            return emailSenderInstance;
+        }
+
+    }
+
+
+    public class EmailSenderViaAzureQueueAndBlobSerializer : IAzureQueueSerializer<EmailModel>
+    {
+        public byte[] Serialize(EmailModel model)
+        {
+            return model.ToContract().ToJson().ToUtf8Bytes();
+        }
+    }
+}
---
+++ src/EmailSender.AzureQueueAndBlob.Publisher/EmailSenderViaQueueContract.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using Common;
+using Lykke.EmailSender;
+
+namespace EmailSender.AzureQueueAndBlob.Publisher
+{
+    public class EmailSenderViaQueueContract
+    {
+
+        public class EmailAttachmentContract
+        {
+            public string FileName { get; set; }
+            public string Mime { get; set; }
+            public string Data { get; set; }
+
+        }
+
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsHtml { get; set; }
+
+        public EmailAttachmentContract[] Attachments { get; set; }
+    }
+
+
+    public static class EmailSenderViaQueueContractMethodExts
+    {
+
+        public static EmailSenderViaQueueContract ToContract(this EmailModel src)
+        {
+            return new EmailSenderViaQueueContract
+            {
+                Email = src.Email,
+                Body = src.Body,
+                Subject = src.Subject,
+                IsHtml = src.IsHtml,
+                Attachments = src.Attachments?.Select(itm => itm.ToContract()).ToArray()
+            };
+        }
+
+
+        public static EmailSenderViaQueueContract.EmailAttachmentContract ToContract(this EmailAttachment src)
+        {
+            return new EmailSenderViaQueueContract.EmailAttachmentContract
+            {
+                Mime = src.Mime,
+                FileName = src.FileName,
+                Data = src.Data.ToBase64()
+            };
+        }
+
+
+        public static EmailModel ToDomain(this EmailSenderViaQueueContract src)
+        {
+            return new EmailModel
+            {
+                Body = src.Body,
+                Subject = src.Subject,
+                Email = src.Email,
+                Attachments = src.Attachments?.Select(itm => itm.ToDomain()).ToArray()
+            };
+        }
+
+        public static EmailAttachment ToDomain(this EmailSenderViaQueueContract.EmailAttachmentContract src)
+        {
+            return new EmailAttachment
+            {
+                FileName = src.FileName,
+                Mime = src.Mime,
+                Data = Convert.FromBase64String(src.Data) 
+            };
+        }
+
+    }
+}
+
